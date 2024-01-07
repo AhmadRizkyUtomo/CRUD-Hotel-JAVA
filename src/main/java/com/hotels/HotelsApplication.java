7408a51a5f4b47ea3709e0e5d13f41fec2c22887
ahmadrizky <2112500356@student.budiluhur.ac.id>
2024-01-07 23:31:18 +0700

    feat: add sample index html

--- src/main/java/com/hotels/HotelsApplication.java
+++ src/main/java/com/hotels/HotelsApplication.java
@@ -2,12 +2,18 @@ package com.hotels;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class HotelsApplication {
+public class HotelsApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HotelsApplication.class, args);
+	}
+
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/").setViewName("index.html");
 	}
 
 }
